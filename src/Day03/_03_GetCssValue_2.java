041f99588a0ee7f666eee2597dd97d899226801d
nese <ilknurnese@gmail.com>
2024-11-10 11:39:53 +0000

    BaseDriver modified

--- src/Day03/_03_GetCssValue_2.java
+++ src/Day03/_03_GetCssValue_2.java
@@ -1,4 +1,36 @@
 package Day03;
 
-public class _03_GetCssValue_2 {
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _03_GetCssValue_2 extends BaseDriver {
+    public static void main(String[] args) {
+
+        Setup();
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.snapdeal.com/");
+
+        WebElement searchBox= driver.findElement(By.id("inputValEnter"));
+
+        //provides value of the parameter
+
+        System.out.println("searchBox.getAttribute(\"class\") = " + searchBox.getAttribute("class"));
+
+
+        //provides the values that shapes the elements
+
+        System.out.println("searchBox.getCssValue(\"color\") = " + searchBox.getCssValue("color"));
+        System.out.println("searchBox.getCssValue(\"background\") = " + searchBox.getCssValue("background"));
+        System.out.println("searchBox.getCssValue(\"font-size\") = " + searchBox.getCssValue("font-size"));
+        System.out.println("searchBox.getCssValue(\"border\") = " + searchBox.getCssValue("border"));
+        System.out.println("searchBox.getCssValue(\"font-family\") = " + searchBox.getCssValue("font-family"));
+
+        TearDown();
+
+    }
 }
--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,32 +1,26 @@
 package Utility;
 
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class BaseDriver {
 
     public static WebDriver driver;
-    public static WebDriverWait wait;
-    public static JavascriptExecutor js;
 
-    static
+
+    public static void Setup()
     {
         driver=new ChromeDriver();
 
-        wait=new WebDriverWait(driver, Duration.ofSeconds(20));
-        js=(JavascriptExecutor)driver;
-
         //driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
-        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
     }
 
-    public static void tearDown( )
+    public static void TearDown( )
     {
         MyFunc.Bekle(3);
         driver.quit();
