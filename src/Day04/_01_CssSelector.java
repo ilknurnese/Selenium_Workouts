c215f328bc0b07b2c8676eb09a54d4ebedbed693
nese <ilknurnese@gmail.com>
2024-11-18 12:25:52 +0000

    added Notes and Q&A

--- src/Day04/_01_CssSelector.java
+++ src/Day04/_01_CssSelector.java
@@ -1,13 +1,25 @@
 package Day04;
 
+import Utility.BaseDriver;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-public class _01_CssSelector {
+public class _01_CssSelector extends BaseDriver {
 
     @Test
     public void Test1(){
 
+// 1- go to https://formsmarts.com/form/yu?mode=h5
+// 2- Select Business .
+// 3- Click discover XYZ snd choose online Advertising
+// 4- Choose Every day
+// 5- Choose Good
+// 6- Click to using XYZ and select the 3rd option
 
+        driver.get("https://formsmarts.com/form/yu?mode=h5");
+
+        WebElement business= driver.findElement(By.cssSelector(""));
 
 
     }
